07639d7f8b1c071e97101dd58faab9bb25602c3d
uglyCode <appostnikov@gmail.com>
2019-03-03 23:20:30 +0300

    Updates

---
+++ precache-manifest.1333c6f9844ca3cdd811083c018515d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33392acfcadd8c663457",
+    "url": "/face-recon/static/js/runtime~main.33392acf.js"
+  },
+  {
+    "revision": "712cbddc3724437a510c",
+    "url": "/face-recon/static/js/main.712cbddc.chunk.js"
+  },
+  {
+    "revision": "00f30b6e578bdf805150",
+    "url": "/face-recon/static/js/2.00f30b6e.chunk.js"
+  },
+  {
+    "revision": "712cbddc3724437a510c",
+    "url": "/face-recon/static/css/main.cb3e5211.chunk.css"
+  },
+  {
+    "revision": "00f30b6e578bdf805150",
+    "url": "/face-recon/static/css/2.2c09bcbc.chunk.css"
+  },
+  {
+    "revision": "be71fab7730474bda7716f6efe0d4026",
+    "url": "/face-recon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb53eb388a8f860245fdd9360f590033.js"
+  "/face-recon/precache-manifest.1333c6f9844ca3cdd811083c018515d5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/face-recon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
